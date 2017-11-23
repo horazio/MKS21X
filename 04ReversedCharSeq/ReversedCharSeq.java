9bf0a6f880bfa690ca0b13941fb2bcee522079af
horazio <hfusco@stuy.edu>
2017-11-23 03:30:58 -0500

    added constructer and reverse method

---
+++ 04ReversedCharSeq/ReversedCharSeq.java
@@ -0,0 +1,20 @@
+public class ReversedCharSeq implements CharSequence{
+    String data;
+    public ReversedCharSeq(String seq){
+	data = seq;
+    }
+
+    public static String reverse(String word){
+        String reversed = "";
+	for(int i = 0; int < data.length(); int++){
+	    reversed = data.charAt(int);
+	    
+	}
+
+    }
+
+
+
+
+
+}
