e66d8594e200720fa1017227769a853c410f5cbf
horazio <hfusco@stuy.edu>
2017-11-28 16:40:29 -0500

    wrote in Circulating book class

--- 05Library/CirculatingBook.java
+++ 05Library/CirculatingBook.java
@@ -1,6 +1,30 @@
 public class CirculatingBook extends LibraryBook{
+    String currentHolder;
+    String dueDate;
+    public CirculatingBook(String au, String ti, String is, String ca){
+	super(au, ti, is, ca);
+    }
 
+    public void checkout(String person, String due){
+	currentHolder = person;
+	dueDate = due;
+    }
 
+    public void returned(){
+	currentHolder = null;
+	dueDate = null;
+    }
 
+    public String circulationStatus(){
+	if (currentHolder==null){
+	    return "The book is on the shelves";
+	} else {
+	    return currentHolder;
+	}
+    }
+
+    public String toString(){
+	return super.toString() + " , " + currentHolder + " , " + dueDate;
+    }
 
 }
