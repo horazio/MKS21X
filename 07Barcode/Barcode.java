97843f0729e22cccd9c0f6427ba6d2ab6de41506
horazio <hfusco@stuy.edu>
2017-12-04 21:49:44 -0500

    fixed a bug in toString

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -6,7 +6,11 @@ public class Barcode implements Comparable<Barcode>{
             throw new IllegalArgumentException();
         }
         
-        int foo = Integer.parseInt(zipCode);
+        try {
+            int foo = Integer.parseInt(zipCode);
+        }catch(NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
         zip = zipCode;
     }
     public String getZip(){
@@ -29,14 +33,15 @@ public class Barcode implements Comparable<Barcode>{
     private int getCSum(){
         int sum = 0;
         for(int i = 0; i < 5; i++){
-            sum += zip.charAt(i) - 30;
+            sum += zip.charAt(i) - 48;
         }
         return sum % 10;
     }
     public String getCode(){
         String code = "|";
         for(int i = 0; i < 5; i++){
-            code += getChar(zip.charAt(i) - 30); 
+            System.out.println(code);
+            code += getChar(zip.charAt(i) - 48); 
         }
         return code + getChar(this.getCSum()) + '|';
     }
@@ -50,8 +55,12 @@ public class Barcode implements Comparable<Barcode>{
     }
     
     public String toString(){
-        return zip + '[' + this.getCode() + ']';
+        return this.getCode() + '(' + zip + ')';
     }
     
-    
+    public static void main(String[] args){
+        Barcode a = new Barcode("92010");
+        System.out.println(getChar(9));
+        System.out.println(a);
+    }
 }
