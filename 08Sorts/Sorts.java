8bc89bccf39ea8f62c92e6e1b22f7075a2a58fd4
horazio <hfusco@stuy.edu>
2017-12-10 10:35:36 -0500

    polishing up testing cases, testing all cases and added bubble sort

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -53,32 +53,49 @@ public class Sorts{
   
   
     public static void main(String[] args){
-        
-        
-        
-	int[] a = {1, 2,1234, 5 ,1235, 4, 1346, 13, -665, -13656,23, 4, 4,4, 4, 4,4 , 3, 4, 5, 123, 1234, 8, 9, 0};
+    long startTime = System.currentTimeMillis();
+    int[] randish = new int[1000];
+    //int[] randish = new int[0];
+    //int[] randish = new int[1];
+    //int[] randish = new int[2];
+    //int[] randish = new int[3];
+    //int[] randish = new int[100000];
 
-
-
-    String input = "[";
-    for (int i=0;i<a.length;i++){
-      input += a[i];
-      if (i!=a.length-1){
-        input += ",";
-      }
+    for(int i = 0 ; i < randish.length; i++){ //PURELY RANDOM
+      randish[i] = (int)(Math.random()*100);
     }
-    input += "]";
-    System.out.println(input);
-    Sorts.bubbleSort(a);
-    System.out.println("-----Sorted Below----");
-    String output = "[";
-    for (int i=0;i<a.length;i++){
-      output += a[i];
-      if (i!=a.length-1){
-        output += ",";
-      }
+
+    //for(int i = 0 ; i < randish.length; i++){ //ALL THE SAME NUMBER OR YOU CAN JUST RUN CODE WITHOUT ANY FOR LOOPS
+    //  randish[i] = 1;
+    //}
+
+    //for(int i = 0 ; i < randish.length; i++){ //BIGGEST TO SMALLEST
+    //  randish[i] = randish.length - i;
+    //}
+
+    //for(int i = 0 ; i < randish.length; i++){ // SMALLEST to BIGGEST
+    //  randish[i] = i;
+    //}
+
+    System.out.println(Arrays.toString(randish));
+
+    //Sorts Tests Here:
+
+    //System.out.println("selectionSort:");
+    //selectionSort(randish);
+
+    //System.out.println("insertionSort:");
+    //insertionSort(randish);
+
+    System.out.println("bubbleSort:");
+    bubbleSort(randish);
+
+    System.out.println(Arrays.toString(randish));
+
+    System.out.println("Runtime: " +
+                       (System.currentTimeMillis() - startTime) + "ms or " +
+                       (System.currentTimeMillis() - startTime)/1000.0 + "s"
+                       );
+    
     }
-    output += "]";
-    System.out.println(output);
-  }
 }
