e072b95269d2006d285836258ce9efed8ea7557c
horazio <hfusco@stuy.edu>
2017-12-13 21:19:55 -0500

    structured a bare bones gui window, no functionality yet

---
+++ 09GUI/TempConvert.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TempConvert extends JFrame implements ActionListener {
+    private JButton b1;
+    private JButton b2;
+    private JTextField t;
+    private Container box;
+    public TempConvert(){
+        this.setTitle("Temperature converter");
+        this.setSize(600,400);
+        this.setLocation(100,100);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        box = this.getContentPane();
+        box.setLayout(new FlowLayout());
+
+        b1 = new JButton("Convert to celsius");
+        b2 = new JButton("convert to farenheit");
+        t = new JTextField(10);
+
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        t.addActionListener(this);
+        
+        
+        box.add(b1);
+        box.add(b2);
+        box.add(t);
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        String s = e.getActionCommand();
+        System.out.println(s);
+    }
+
+
+    public static void main(String[] args){
+        TempConvert t = new TempConvert();
+        t.setVisible(true);
+    }
+}
